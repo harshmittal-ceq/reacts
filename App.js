57d580f949b78f55faa64ed1cc11cc9c5bcab1bc
Harsh Mittal <mittalharsh4321@gmail.com>
2024-07-25 20:48:52 +0530

    3rd

--- App.js
+++ App.js
@@ -1,27 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement("div",{id:"child"},[
-            React.createElement("h1",{},"hi this is hars"),
-            React.createElement("h2",{},"h2")
-        ]),
-        React.createElement("div",{id:"child"},[
-            React.createElement("h1",{},"hi"),
-            React.createElement("h2",{},"h2")
-        ])
-    ]
+// React.createElemet => React Element- JS Object => HTMLElement 
+
+// const heading = React.createElement("h1",{id:"heading"},(
+//     React.createElement("div",{},"hi")
+// ))
+
+//JSX is not HTML in JS . it is html like syntax.
+// Transpiled before it reaches the JSX - Babel
+//JSX => babel transpiles it to React.createElement => Reactelement-JS object => HTMLElement(Render)
+const jsxHeading = (<h1 id="heading" className="heading">
+    This is jsx
+    </h1>)
+console.log(jsxHeading)
+
+const Title =()=>(
+    <h1 className="head" tabIndex="5">
+        JSX with tabindex
+    </h1>
+)
+//class based component - old
+//functional component - new
+
+//React Functional Component - A function that return jsx or react element
+// const HeadingComponent = () =>{
+//     return <h1>Hi harsh</h1>
+// }
+
+// const HeadingComponent = () =><h1>Hi harsh</h1>
+//jsx sanitize the data if any malicious data come from api
+
+//Component composition mneans component inside component
+const number =100
+const HeadingComponent = () =>(
+    <div id="container">
+        {Title()}
+        {jsxHeading}
+        <h1>{number*100}</h1>
+        <h1>Hi harsh</h1>
+        <Title/>
+    </div>
 )
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World from React!"
-)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent);
+root.render(<HeadingComponent/>)
